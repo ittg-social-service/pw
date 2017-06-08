375d83abcfbfec0b00d81d41de7f48d9cf86a606
EliverLara <eliverlara@gmail.com>
2017-06-08 16:43:40 -0500

    :bug: agregar middleware auth a los controladores

--- app/Http/Controllers/ReferenceTypeController.php
+++ app/Http/Controllers/ReferenceTypeController.php
@@ -11,6 +11,14 @@ class ReferenceTypeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+   public function __construct()
+    {
+        $this->middleware('auth');
+
+
+    }
+    
     public function index()
     {
 
--- app/Http/Controllers/SemesterController.php
+++ app/Http/Controllers/SemesterController.php
@@ -12,6 +12,14 @@ class SemesterController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+      public function __construct()
+    {
+        $this->middleware('auth');
+
+
+    } 
+    
     public function index()
     {
         return view('semester.index');
